f3296f117b6e0020d7d5f8200f5c7c1d94ea80da
BMRagot <bastien.marichal@gmail.com>
2014-02-20 10:21:20 +0100

    set mention popover  position

--- js/main.js
+++ js/main.js
@@ -19,34 +19,65 @@ var map = L.mapbox.map('map', 'tarraschk.gm6bpp19', {
 	CanvasRenderingContext2D.prototype.clear = function() {
 		this.clearRect(0, 0,10000,10000);// this.canvas.clientWidth, this.canvas.clientHeight);
 	};
+
+	CanvasRenderingContext2D.prototype.makeBlop2= function(xs,ys,r1,r2,h,w){	
+		
+		var d=4*(r1+r2)*(r1+r2)-8*(r1*r1+r2*r2-2*h*h-w*w),
+			sol=(-2*(r1+r2)-Math.sqrt(d))/4>0/* ?(-2*(r1+r2)-Math.sqrt(d))/4:(-2*(r1+r2)+Math.sqrt(d))/4 */,
+			c=Math.sqrt(sol*sol+r1*r1-h*h);
+		
+		alpha1= Math.acos((L/2)/Math.sqrt(h*h/4+L*L/4));
+//			alpha2= Math.acos((L/4)/Math.sqrt(h*h/4+L*L/16));
+			alpha2= Math.acos((L/4)/Math.sqrt(h*h/4+L*L*0.25/4));
+		console.log(sol);
+		this.fillStyle="green";
+		
+		this.beginPath();
+		this.arc(xs,ys+h,r1,0,2*Math.PI,true);
+		this.fill();
+		this.beginPath();
+		this.arc(xs+w,ys+h,r2,0,2*Math.PI,true);
+		this.fill();
+				this.fillStyle="red";
+
+		this.beginPath();
+		this.arc(xs+c,ys,sol,0,2*Math.PI,true);
+
+		this.fill();
+
+	}
 	
-	CanvasRenderingContext2D.prototype.makeBlop= function(xs,ys,L,h,a){	
-		var r=h/2*a,
+	CanvasRenderingContext2D.prototype.makeBlop= function(xs,ys,L,h,a,b){	
+	//	var b=0.5,
+		var	r=h/2*a,
 			alpha1= Math.acos((L/2)/Math.sqrt(h*h/4+L*L/4)),
-			alpha2= Math.acos((L/4)/Math.sqrt(h*h/4+L*L/16));
+//			alpha2= Math.acos((L/4)/Math.sqrt(h*h/4+L*L/16));
+			alpha2= Math.acos((L/4)/Math.sqrt(h*h/4+L*L*b*b/4));
 			
 		this.beginPath();
 		this.arc(xs+L/2,ys,r,Math.PI-alpha1,alpha2,true);
-		this.arc(xs+3*L/4,ys+h/2,Math.sqrt(h*h/4+L*L/16)-r,Math.PI+alpha2,Math.PI-alpha2);
+//		this.arc(xs+3*L/4,ys+h/2,Math.sqrt(h*h/4+L*L/16)-r,Math.PI+alpha2,Math.PI-alpha2);
+		this.arc(xs+(1+b)*L/2,ys+h/2,Math.sqrt(h*h/4+L*L*b*b/4)-r,Math.PI+alpha2,Math.PI-alpha2);
 		this.arc(xs+L/2,ys+h,r,-alpha2,Math.PI+alpha1,true);
 		this.arc(xs,ys+h/2,Math.sqrt(h*h+L*L)/2-r,alpha1,2*Math.PI-alpha1);
 		//this.stroke();
 		this.fill();
 	}
 	
-	CanvasRenderingContext2D.prototype.makeLink= function(a,b,h,coef){	
+	CanvasRenderingContext2D.prototype.makeLink= function(a,b,h,coef1,coef2){	
 
 		var aposY =  $("#arpeople ."+a).offset().top - $("#equipe").offset().top+$("#arpeople ."+a).width()/2;//100;
 		var aposX =  $("#arpeople ."+a).offset().left +$("#arpeople ."+a).width()/2;//100;
 		var bposY = $("#arpeople ."+b).offset().top - $("#equipe").offset().top+$("#arpeople ."+b).width()/2;//100;
 		var bposX = $("#arpeople ."+b).offset().left +$("#arpeople ."+b).width()/2;//100;
 		
-		L=Math.sqrt(Math.pow(aposX-bposX,2)+Math.pow(aposY-bposY,2))*4/3;
-		alpha= Math.acos(-(aposX-bposX)/(L*3/4));
+		L=Math.sqrt(Math.pow(aposX-bposX,2)+Math.pow(aposY-bposY,2))*2/(1+coef2);
+		alpha= Math.acos(-(aposX-bposX)/(L*(1+coef2)/2));
 		
 		this.translate(aposX, aposY);
 		this.rotate(alpha);
-		this.makeBlop(0,-h/2,L,h,coef);
+		this.makeBlop(0,-h/2,L,h,coef1,coef2);
+		//this.makeBlop2(0,-h,$("#arpeople ."+a).width()/2,$("#arpeople ."+b).width()/2,h,L);
 		this.rotate(-alpha);
 		this.translate(-aposX,-aposY);
 	}
@@ -74,24 +105,26 @@ var map = L.mapbox.map('map', 'tarraschk.gm6bpp19', {
 		var w = this.canvas.clientWidth,
 			h = this.canvas.clientHeight,
 			cx = w/2,
-			cy = (h-40)/2;
+			cy = (h-40)/2,
+			R=$('#bg-logo-container img').width()/2+5;
 	
 		this.lineJoin="round";
 		this.lineCap="round";
 	
-		var grd=this.createRadialGradient(cx,cy,$('#bg-logo-container img').width()/2+5,cx,cy,1000);
+		/*
+var grd=this.createRadialGradient(cx,cy,R,cx,cy,1000);
 		grd.addColorStop(0,"rgb(200,200,200)");
 		grd.addColorStop(1,"white");
+*/
 		
-		this.save();
 
-		this.fillStyle = grd;
+		this.fillStyle ="rgb(200,200,200)";// grd;
 		this.fillRect(0,0,w,h);
 		
 /* 		this.makePattern() */
 		
 		this.beginPath();	
-		this.arc(cx,cy,$('#bg-logo-container img').width()/2+5,0,2*Math.PI);
+		this.arc(cx,cy,R,0,2*Math.PI);
 		this.closePath();
 		this.strokeStyle="rgb(210,210,210)";
 		this.lineWidth=30;
@@ -103,22 +136,23 @@ var map = L.mapbox.map('map', 'tarraschk.gm6bpp19', {
  		
 		/*  link between bubble only if desktop  */
  		if($(window).width()>992){//1233){
- 			this.fillStyle="rgb(156,156,156)";
-			this.makeLink("ar-gauthier-blin","ar-florian-wininger",250,0.5);   
-			this.fillStyle="rgb(200,200,200)";
-			this.makeLink("ar-florian-wininger","ar-antoine-fond" ,720,0.9);   
-			this.fillStyle="rgb(200,200,200)";
-			this.makeLink("ar-maxime-alay-eddine","ar-arthur-feral",590,0.8);   		
-			this.fillStyle="rgb(120,120,120)";
-			this.makeLink("ar-maxime-alay-eddine","ar-raphael-sfeir",260,0.7);   
+ 			/*
+this.fillStyle="rgb(156,156,156)";
+			this.makeLink("ar-gauthier-blin","ar-florian-wininger",$(".ar-grid li").width()/2,0.9,0.8);   
+			this.fillStyle="rgb(51,138,199)";
+			this.makeLink("ar-florian-wininger","ar-antoine-fond" ,$(".ar-grid li").width()/2,0.9,0.5);   
+			this.fillStyle="rgb(180,180,180)";
+			this.makeLink("ar-maxime-alay-eddine","ar-arthur-feral",590,0.8,0.5);   		
+			this.fillStyle="rgb(51,138,199)";
+			this.makeLink("ar-maxime-alay-eddine","ar-raphael-sfeir",260,0.7,0.5); 
+*/
 
  		}
  		
 		this.globalCompositeOperation = "destination-out";
 		this.beginPath();
-		this.arc(cx,cy,$('#bg-logo-container img').width()/2+5,0,2*Math.PI);
+		this.arc(cx,cy,R,0,2*Math.PI);
 		this.fill();
-		this.restore();
 		this.globalCompositeOperation = "source-over";
 
 	}
@@ -146,17 +180,24 @@ function teamSetting(callback){
 		wa=w/2-R,
 		h = $('.ar-grid').height();
 		
+		
 	if(w>992){
-		var rb = Math.min(wa/2,h/3),
-			mg = 0.15*rb;
+		var rb =2*Math.sqrt(wa*h/(8*Math.PI)),// Math.min(wa/2,h/3),
+			mgw = 0.15*rb,
+			mgh = 0.05*rb;
+			console.log("ff");
+			
+			console.log(rb);
+			console.log( Math.min(wa/2,h/3));
+			
 		$(".ar-grid li ").css('width',rb).css('height',rb);
 		
-		$(".ar-column-1.ar-row-1").css('left',eval(wa-rb-mg)).css('top',eval(mg));;
-		$(".ar-column-2.ar-row-1").css('left',eval(wa+2*R+mg)).css('top',eval(mg));;
-		$(".ar-column-1.ar-row-2").css('left',eval(mg)).css('top',eval(rb));
-		$(".ar-column-2.ar-row-2").css('left',eval(w-rb-mg)).css('top',eval(rb));
-		$(".ar-column-1.ar-row-3").css('left',eval(wa-rb+2*mg)).css('top',eval(2*rb-mg));
-		$(".ar-column-2.ar-row-3").css('left',eval(wa+2*R-2*mg)).css('top',eval(2*rb-mg));
+		$(".ar-column-1.ar-row-1").css('left',eval(wa-rb-mgw)).css('top',eval(mgh));;
+		$(".ar-column-2.ar-row-1").css('left',eval(wa+2*R+mgw)).css('top',eval(mgh));;
+		$(".ar-column-1.ar-row-2").css('left',eval(mgw)).css('top',eval(rb));
+		$(".ar-column-2.ar-row-2").css('left',eval(w-rb-mgw)).css('top',eval(rb));
+		$(".ar-column-1.ar-row-3").css('left',eval(wa-rb+2*mgw)).css('top',eval(h-rb-mgh));
+		$(".ar-column-2.ar-row-3").css('left',eval(wa+2*R-2*mgw)).css('top',eval(h-rb-mgh));
 	
 	}else{
 		
@@ -188,10 +229,8 @@ function teamSetting(callback){
 	function resizeCanvas(){
 		canvas.width = $('#equipe').width();
 		canvas.height = $('#equipe').height();
-	/*
-	teamSetting(function(){
+		teamSetting(function(){
 		ctx.createTeamMask();});
-*/
 	}
 
     window.addEventListener('resize', resizeCanvas, false);
@@ -259,26 +298,11 @@ function teamSetting(callback){
 /*  Popover #mention, placement improvement*/
     $('#mention').popover({html:"true",container: 'body',placement:'left'});
 	$('#mention').on('shown.bs.popover', function () {
-		/*
-console.log($('.popover').position().top);
 
-		console.log($('.popover').width());
-		console.log($('.popover-content').height());
+		$('.popover').css('top',$('#mention').offset().top-$('.popover-content').height()-30);
+		$('.popover').css('right',0);
+		$('.popover .arrow').css('top',$('.popover').height()-$('#mention').height());
 		
-		var w=$(window).width()*0.90,
-			h=$(window).height()-$('.popover-content').height();
-		$('.popover').css('width',w);
-		$('.popover').css('top',h);
- 		console.log($('.popover').position().top);
-*/
-
- 
- /*
- 
- $('.popover').css('top', parseInt($('.popover').css('top'))-parseInt($('.popover').css('height'))+'px');//   -($(window).height()-parseInt($('.popover').css('height'))  )+ 'px');
-  $('.popover').css('left', $(window).width()-parseInt($('.popover').css('width'))-5+'px');
-
-*/
 })
 
 /*     change the content of contact section depending on device */
@@ -291,7 +315,7 @@ console.log($('.popover').position().top);
 	}
    
 /*    HACK :go back to page top on refresh  */ /* AMODFIFIER */
-		$('html, body').animate({scrollTop:0}, 'fast'); 		
+/* 		$('html, body').animate({scrollTop:0}, 'fast'); 		 */
 
  /*AJOUTER CONDITION POUR ONLY DESKTOP DEVICE AND NO SPLASCHSCRENN ANIMATION IF NOT AT TOP*/
    (new TimelineLite({onComplete:initScrollAnimations}))				
