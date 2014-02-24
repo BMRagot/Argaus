740017626823d4fdf730fb6c1e7864b1d7dfe5be
BMRagot <bastien.marichal@gmail.com>
2014-02-24 14:48:08 +0100

    improve scrolling

--- js/main.js
+++ js/main.js
@@ -176,6 +176,56 @@ $('.ar-grid').css('top',$('#arpeople').position().top);
 ////////////
 
 
+/* OK fullpage.js incompatible en tant que tel avec scrolorama */
+
+/*  $.fn.fullpage(); */
+/*  { */
+       
+/*                easing: 'none' */
+
+       /*
+ verticalCentered: true,
+        resize : true,
+        slidesColor : ['#ccc', '#fff'],
+        anchors:['firstSlide', 'secondSlide'],
+        scrollingSpeed: 700,
+        easing: 'easeInQuart',
+        menu: false,
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        autoScrolling: true,
+        scrollOverflow: false,
+        css3: false,
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#element1, .element2',
+        normalScrollElements: '#element1, .element2',
+        keyboardScrolling: true,
+        touchSensitivity: 15,
+        continuousVertical: false,
+        animateAnchor: true,
+*/
+
+        /*
+//events
+        onLeave: function(index, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+*/
+/*     }); */
+
+
+
+
+
 function teamSetting(callback){
 	var R =	$('#bg-logo-container img').width()/2+5, /* rayon du trou central */
 		w = $(window).width(),
@@ -279,17 +329,24 @@ ctx.clear();
             }, 800);
         }
     });
-    menu.find('a').click(function(){
-        if(menuCollapsible.hasClass('in'))
+    menu.find('a').click(function(e){
+    	e.preventDefault();
+    	if(menuCollapsible.hasClass('in'))
             menuCollapsible.collapse('hide');
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top - 45
-        }, 800);
+    	if(this.id=='top-button'){	
+    			  $('html, body').animate({scrollTop:0},800,'easeInOutExpo');
+		}else{
+	        $('html, body').animate({
+	            scrollTop: $( $.attr(this, 'href') ).offset().top - 45
+	        }, 1600,'linear');
+        }
         return false;
     });
+
 /*     Scroll to top when click on footer logo */
-    $('#footer-logo').click(function(){
-    	 $('html, body').animate({scrollTop:0 }, 800);
+    $('#footer-logo').click(function(e){
+        e.preventDefault();
+    	$('html, body').animate({scrollTop:0 }, 800,'easeInOutExpo');
     	return false;
     });
 
@@ -355,7 +412,7 @@ $(window).resize(function () {
 	});
 */
 	function initScrollAnimations() {
-	
+/* 	if(true){	 */
 	 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		 
 /* 		 pas deffet de scrolling */
